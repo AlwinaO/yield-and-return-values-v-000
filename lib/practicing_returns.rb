878bdd814968c0aaa4b6411cc90711a7356991aa
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-01-05 02:53:17 +0000

    automatically backed up by learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -4,11 +4,12 @@ def hello(array)
   i = 0
   collection = [ ]
   while i < array.length
+    #can add binding.pry here to manually execute yield(array[i])
     collection << yield(array[i])
     i += 1
   end
   collection
 end
 
-
+#can add binding.pry here to manually invoke #hello and see the return value
 hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
